3867dd70b5ada55c068f2924bcf20b65055bc286
augustolopeslyra@gmail.com <augustolopeslyra@gmail.com>
2025-08-13 20:11:18 -0300

    Adicionando botão de editar perfil na tela de perfil

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -49,6 +49,13 @@ const ProfileScreen: React.FC = () => {
           )}
         </ProfileCard>
 
+          <Button
+          title="Editar Perfil"
+          onPress={() => navigation.navigate('EditProfile' as any)}
+          containerStyle={styles.button as ViewStyle}
+          buttonStyle={styles.editButton}
+        />
+
         <Button
           title="Voltar"
           onPress={() => navigation.goBack()}
@@ -81,6 +88,10 @@ const styles = {
   },
   logoutButton: {
     backgroundColor: theme.colors.error,
+    paddingVertical: 12,
+  },
+  editButton: {
+    backgroundColor: theme.colors.success,
     paddingVertical: 12,
   },
 };
